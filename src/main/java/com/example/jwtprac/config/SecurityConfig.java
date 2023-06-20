7e99ae6e612df553aa5559a83108ad9deac108aa
jay <wlguswhd0809@naver.com>
2023-06-20 19:13:47 +0900

    [ADD] Config

--- src/main/java/com/example/jwtprac/config/SecurityConfig.java
+++ src/main/java/com/example/jwtprac/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.jwtprac.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity // 기본적인 웹 보안 활성화
