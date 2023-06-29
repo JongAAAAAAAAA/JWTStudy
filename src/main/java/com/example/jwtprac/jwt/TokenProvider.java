6bc8435cee120afbfe038d92107cb025cd276d86
jay <wlguswhd0809@naver.com>
2023-06-29 15:13:27 +0900

    [ADD] getAuthentication & validateToken

--- src/main/java/com/example/jwtprac/jwt/TokenProvider.java
+++ src/main/java/com/example/jwtprac/jwt/TokenProvider.java
@@ -1,31 +1,35 @@
 package com.example.jwtprac.jwt;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Component
 public class TokenProvider implements InitializingBean { // 모든 속성이 BeanFactory에 의해 설정되면 반응해야 하는 빈에 의해 구현되는 인터페이스
     private final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
 
     private static final String AUTHORITIES_KEY = "auth";
-
     private final String secret;
     private final long tokenValidityInMilliseconds;
-
     private Key key;
 
     public TokenProvider(
@@ -52,12 +56,55 @@ public class TokenProvider implements InitializingBean { // 모든 속성이 Bea
         long now = new Date().getTime();
         Date validity = new Date(now + this.tokenValidityInMilliseconds); // token의 expire time 설정
 
-        return Jwts.builder()
-                .setSubject(authentication.getName())
-                .claim(AUTHORITIES_KEY, authorities)
-                .signWith(key, SignatureAlgorithm.HS512)
-                .setExpiration(validity)
-                .compact();
+        return Jwts.builder() //JWT를 생성하기 위한 빌더 객체
+                .setSubject(authentication.getName()) // 토큰의 주제를 나타내는 값
+                .claim(AUTHORITIES_KEY, authorities) // 추가적인 클레임 설정, claim : payload에 포함되는 정보
+                .signWith(key, SignatureAlgorithm.HS512) // JWT를 서명하기위해 Key, Algorithm 지정
+                .setExpiration(validity) // 만료 시간
+                .compact(); // 마지막으로 JWT를 압축하고 서명하여 최종적인 JWT 문자열을 생성
+    }
+
+    // JWT 토큰을 복호화하여 토큰에 들어있는 정보를 꺼내는 코드
+    // 토큰으로 클레임을 만들고 이를 이용해 유저 객체를 만들어서 최종적으로 authentication 객체를 리턴
+    // 인증 정보 조회
+    public Authentication getAuthentication(String token) {
+        // 토큰 복호화 메소드
+        Claims claims = Jwts
+                .parserBuilder() // JWT를 파싱하기 위한 파서 빌더 객체를 생성
+                .setSigningKey(key) // JWT를 검증하기 위해 사용할 서명 키를 설정
+                .build() // JWT parser 객체 생성
+                .parseClaimsJws(token) // 주어진 토큰을 해석하고 검증
+                .getBody(); // 검증된 JWT의 Claim(paylod) 정보를 추출
+
+        // 클레임 권한 정보 가져오기
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        // User 객체를 만들어서 Authentication 리턴
+        User principal = new User(claims.getSubject(), "", authorities);
+
+        // 인증된 생성자인 Authentication 생성
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+    }
+
+    // 토큰의 유효성 검증을 수행
+    public boolean validateToken(String token) {
+        try {
+            // 주어진 토큰을 해석하고 검증
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+        }
+        return false;
     }
 
 
