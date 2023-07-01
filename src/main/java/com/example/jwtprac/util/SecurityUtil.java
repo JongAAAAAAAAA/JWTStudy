86ba84cb91566d4be70c8f2495f1f3a749eae9f7
jay <wlguswhd0809@naver.com>
2023-07-01 20:33:33 +0900

    [ADD] SecurityUtil

---
+++ src/main/java/com/example/jwtprac/util/SecurityUtil.java
@@ -0,0 +1,33 @@
+package com.example.jwtprac.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+@Slf4j
+public class SecurityUtil {
+    // SecurityContext 에서 Authentication 객체를 이용해 username 을 리턴
+    public static Optional<String> getCurrentUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null) {
+            log.debug("Security Context에 인증 정보가 없습니다.");
+            return Optional.empty();
+        }
+
+        String username = null;
+
+        // UserDetails 의 타입이 Object(UserDetails type) 이거나 String 일 수 있으므로 경우를 나눈다.
+        if (authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
+            username = springSecurityUser.getUsername();
+        } else if (authentication.getPrincipal() instanceof String) {
+            username = (String) authentication.getPrincipal();
+        }
+
+        return Optional.ofNullable(username);
+    }
+}
