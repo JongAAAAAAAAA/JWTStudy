298bf982808023f395cdc97f0bcb044644fb974b
jay <wlguswhd0809@naver.com>
2023-07-04 19:59:50 +0900

    [ADD] SignIn

--- src/main/java/com/example/jwtprac/config/SecurityConfig.java
+++ src/main/java/com/example/jwtprac/config/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig {
                 // 로그인, 회원가입은 토큰이 없는 상태로 요청이 들어오므로 permitAll
                 .antMatchers("/api/authenticate").permitAll() // 토큰을 받기위한 로그인 api
                 .antMatchers("/api/signup").permitAll() // 회원 가입을 위한 api
+                .antMatchers("/api/signin").permitAll() // 로그인을 위한 api
                 .anyRequest().authenticated() // 나머지 요청들은 모두 인증을 받아야 함
 
                 /** JwtSecurityConfig 적용 */
